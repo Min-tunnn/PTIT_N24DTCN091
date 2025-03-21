689146c0b9f6802ec5e4f2e4bbfa1f6bd64f6791
Min-tunnn <minhtuan9168@gmail.com>
2025-03-21 14:13:06 +0700

    Add files via upload

---
+++ Session_21/Bai3/index.js
@@ -0,0 +1,7 @@
+let input_check = prompt("Vui lòng nhập mật khẩu: ");
+let pass = 'Tuan29042006';
+if(input_check==pass){
+    document.write("Welcome!!");
+}else{
+    alert("Error password, pleasy try again!!");
+}
